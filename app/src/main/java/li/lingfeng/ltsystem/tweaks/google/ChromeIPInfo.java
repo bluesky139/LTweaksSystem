0fde8bc7846839449efc40e96275ad9c8236e4d2
bluesky139 <bluesky139@gmail.com>
2019-06-23 09:03:43 +0800

    [Chrome] Use ip-api.com to get IP info.

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/google/ChromeIPInfo.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/google/ChromeIPInfo.java
@@ -74,11 +74,10 @@ public class ChromeIPInfo extends ChromeBase {
                 final String host = new URL(url).getHost();
                 InetAddress address = InetAddress.getByName(host);
                 final String ip = address.getHostAddress();
-                String lang = Locale.getDefault().getLanguage().equals(new Locale("zh")) ? "zh-CN" : "en-US";
+                String lang = Locale.getDefault().getLanguage().equals(new Locale("zh")) ? "zh-CN" : "en";
 
                 Request request = new Request.Builder()
-                        .url("https://clientapi.ipip.net/browser/chrome?ip=" + ip + "&domain=" + host + "&l=" + lang)
-                        .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36")
+                        .url("http://ip-api.com/json/" + ip + "?&lang=" + lang)
                         .build();
 
                 if (mHttpClient == null) {
@@ -88,7 +87,7 @@ public class ChromeIPInfo extends ChromeBase {
                 Response response = mHttpClient.newCall(request).execute();
                 if (response.code() == 200) {
                     final String str = response.body().string();
-                    Logger.i("Got ip info from ipip.net, " + str);
+                    Logger.i("Got ip info, " + str);
                     String html = parseIpInfo(host, ip, str);
                     return Pair.create(true, html);
                 } else {
@@ -100,28 +99,19 @@ public class ChromeIPInfo extends ChromeBase {
         }
 
         private String parseIpInfo(String host, String ip, String str) throws Throwable {
-            JSONObject jAll = (JSONObject) JSON.parse(str);
-            int ret = jAll.getInteger("ret");
-            if (ret != 0) {
-                throw new RuntimeException("parseIpInfo error, ret is not 0.");
+            JSONObject jData = (JSONObject) JSON.parse(str);
+            String ret = jData.getString("status");
+            if (!"success".equals(ret)) {
+                throw new RuntimeException("parseIpInfo error, ret is not success.");
             }
 
             LinkedHashMap<Integer, String> data = new LinkedHashMap<>(5);
             data.put(R.string.chrome_ip_domain, host);
             data.put(R.string.chrome_ip_ip, ip);
 
-            JSONObject jData = (JSONObject) jAll.get("data");
-            data.put(R.string.chrome_ip_geo, jData.getString("country") + " " + jData.getString("province") + " " + jData.getString("city"));
+            data.put(R.string.chrome_ip_geo, jData.getString("country") + " " + jData.getString("regionName") + " " + jData.getString("city"));
             data.put(R.string.chrome_ip_isp, jData.getString("isp"));
-            JSONArray jAsn = jData.getJSONArray("asn");
-            String asn = "";
-            for (int i = 0; i < jAsn.size(); ++i) {
-                if (i != 0) {
-                    asn += "<br>&nbsp;&nbsp;";
-                }
-                asn += jAsn.getString(i);
-            }
-            data.put(R.string.chrome_ip_asn, asn);
+            data.put(R.string.chrome_ip_asn, jData.getString("as"));
 
             final StringBuilder stringBuilder = new StringBuilder();
             for (LinkedHashMap.Entry<Integer, String> kv : data.entrySet()) {
