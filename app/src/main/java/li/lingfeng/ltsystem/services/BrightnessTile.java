7e4c569ccdf3f988e2e751cbdccc6ca60db02f27
bluesky139 <bluesky139@gmail.com>
2019-08-17 09:33:38 +0800

    [Quick Settings] Update tile state, active for manual, inactive for auto.

--- app/src/main/java/li/lingfeng/ltsystem/services/BrightnessTile.java
+++ app/src/main/java/li/lingfeng/ltsystem/services/BrightnessTile.java
@@ -1,7 +1,5 @@
 package li.lingfeng.ltsystem.services;
 
-import android.content.Intent;
-import android.os.IBinder;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
@@ -13,15 +11,11 @@ import li.lingfeng.ltsystem.utils.Logger;
 public class BrightnessTile extends TileService {
 
     @Override
-    public IBinder onBind(Intent intent) {
-        IBinder binder = super.onBind(intent);
+    public void onStartListening() {
         int value = Prefs.instance().getInt(R.string.key_quick_settings_tile_preconfigured_brightness, 0);
         if (value > 0) {
-            Tile tile = getQsTile();
-            tile.setLabel("Set " + value + " brightness");
-            tile.updateTile();
+            updateTile(value);
         }
-        return binder;
     }
 
     @Override
@@ -32,6 +26,19 @@ public class BrightnessTile extends TileService {
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE,
                     Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, value);
+            updateTile(value);
         }
+    }
+
+    private void updateTile(int value) {
+        Tile tile = getQsTile();
+        tile.setLabel("Set " + value + " brightness");
+        try {
+            tile.setState(Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE)
+                    == Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        } catch (Settings.SettingNotFoundException e) {
+            tile.setState(Tile.STATE_UNAVAILABLE);
+        }
+        tile.updateTile();
     }
 }
