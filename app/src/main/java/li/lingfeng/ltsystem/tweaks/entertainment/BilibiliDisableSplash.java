c2afa60c8bb100c70de9d626ff789a0b1dabbb72
bluesky139 <bluesky139@gmail.com>
2019-09-18 09:11:27 +0800

    [Bilibili] Disable splash.

---
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/BilibiliDisableSplash.java
@@ -0,0 +1,26 @@
+package li.lingfeng.ltsystem.tweaks.entertainment;
+
+import android.app.Activity;
+
+import li.lingfeng.ltsystem.ILTweaks;
+import li.lingfeng.ltsystem.R;
+import li.lingfeng.ltsystem.lib.MethodsLoad;
+import li.lingfeng.ltsystem.prefs.PackageNames;
+import li.lingfeng.ltsystem.tweaks.TweakBase;
+import li.lingfeng.ltsystem.utils.Logger;
+
+@MethodsLoad(packages = PackageNames.BILIBILI, prefs = R.string.key_bilibili_disable_splash)
+public class BilibiliDisableSplash extends TweakBase {
+
+    private static final String SPLASH_ACTIVITY = "tv.danmaku.bili.ui.splash.SplashActivity";
+
+    @Override
+    public void android_app_Activity__performCreate__Bundle_PersistableBundle(ILTweaks.MethodParam param) {
+        beforeOnClass(SPLASH_ACTIVITY, param, () -> {
+            Logger.v("Set DisableSplash to true.");
+            Activity activity = (Activity) param.thisObject;
+            activity.getSharedPreferences("bili_main_settings_preferences", 0)
+                    .edit().putBoolean("DisableSplash", true).commit();
+        });
+    }
+}
