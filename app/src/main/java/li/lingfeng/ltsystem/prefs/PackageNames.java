f6739efd9247906ef530e46e6a682383b73d42b3
bluesky139 <bluesky139@gmail.com>
2019-11-25 10:27:46 +0800

    [Quick Edit] Darken starting window.

--- app/src/main/java/li/lingfeng/ltsystem/prefs/PackageNames.java
+++ app/src/main/java/li/lingfeng/ltsystem/prefs/PackageNames.java
@@ -69,6 +69,7 @@ public class PackageNames {
     public static final String SUPER_SU = "eu.chainfire.supersu";
     public static final String NOVA_LAUNCHER = "com.teslacoilsw.launcher";
     public static final String MIXPLORER = "com.mixplorer";
+    public static final String QUICK_EDIT = "com.rhmsoft.edit";
 
     public static final String[] _SYSTEM_BOOT_PACKAGES = new String[] {
             ANDROID,
