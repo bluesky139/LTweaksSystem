9169714373ae82a2a75db88941176101397933e9
bluesky139 <bluesky139@gmail.com>
2019-12-15 18:30:54 +0800

    [Pica Comic] Remove ads.

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/PicaComicAds.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/PicaComicAds.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
+
 import li.lingfeng.ltsystem.ILTweaks;
 import li.lingfeng.ltsystem.R;
 import li.lingfeng.ltsystem.lib.MethodsLoad;
@@ -21,16 +23,49 @@ public class PicaComicAds extends TweakBase {
 
     private static final String MAIN_ACTIVITY = "com.picacomic.fregata.activities.MainActivity";
     private static final String COMIC_VIEWER_ACTIVITY = "com.picacomic.fregata.activities.ComicViewerActivity";
+    private WeakReference<ViewGroup> mComicList;
 
     @Override
     public void android_app_Activity__performCreate__Bundle_PersistableBundle(ILTweaks.MethodParam param) {
         afterOnClass(MAIN_ACTIVITY, param, () -> {
-            Logger.v("Remove popupWebview bannerWebview");
             Activity activity = (Activity) param.thisObject;
-            View popupWebview = ViewUtils.findViewByName(activity, "popupWebview");
+            ViewGroup rootView = (ViewGroup) activity.getWindow().getDecorView();
+
+            Logger.v("Remove popupWebview bannerWebview");
+            View popupWebview = ViewUtils.findViewByName(rootView, "popupWebview");
             ViewUtils.removeView(popupWebview);
-            View bannerWebview = ViewUtils.findViewByName(activity, "bannerWebview");
+            View bannerWebview = ViewUtils.findViewByName(rootView, "bannerWebview");
             ViewUtils.removeView(bannerWebview);
+
+            rootView.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+                ViewGroup comicList = (ViewGroup) ViewUtils.findViewByName(rootView, "recyclerView_comic_list");
+                if (comicList != null && (mComicList == null || mComicList.get() != comicList)) {
+                    Logger.d("comicList " + comicList);
+                    mComicList = new WeakReference<>(comicList);
+                    comicList.setOnHierarchyChangeListener(new ViewGroup.OnHierarchyChangeListener() {
+                        @Override
+                        public void onChildViewAdded(View parent, View child) {
+                            if (child instanceof ViewGroup) {
+                                ViewGroup viewGroup = (ViewGroup) child;
+                                child = viewGroup.getChildAt(0);
+                                if (child instanceof WebView) {
+                                    Logger.v("Remove " + child);
+                                    ViewUtils.removeView(child);
+                                    TextView textView = new TextView(activity);
+                                    textView.setText("   ↓");
+                                    textView.setTextColor(Color.BLACK);
+                                    textView.setTextSize(24);
+                                    viewGroup.addView(textView);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onChildViewRemoved(View parent, View child) {
+                        }
+                    });
+                }
+            });
         });
 
         afterOnClass(COMIC_VIEWER_ACTIVITY, param, () -> {
