1ce603ce29ad03d179c7949960233721652f573c
bluesky139 <bluesky139@gmail.com>
2020-03-21 19:36:52 +0800

    [JD] Remove fullscreen ads.

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/shopping/JDAds.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/shopping/JDAds.java
@@ -1,8 +1,6 @@
 package li.lingfeng.ltsystem.tweaks.shopping;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
 
 import li.lingfeng.ltsystem.ILTweaks;
 import li.lingfeng.ltsystem.R;
@@ -10,31 +8,20 @@ import li.lingfeng.ltsystem.lib.MethodsLoad;
 import li.lingfeng.ltsystem.prefs.PackageNames;
 import li.lingfeng.ltsystem.tweaks.TweakBase;
 import li.lingfeng.ltsystem.utils.Logger;
+import li.lingfeng.ltsystem.utils.ReflectUtils;
 
 @MethodsLoad(packages = PackageNames.JD, prefs = R.string.key_jd_ads)
 public class JDAds extends TweakBase {
 
-    private static final String MAIN_ACTIVITY = "com.jingdong.app.mall.main.MainActivity";
     private static final String MAIN_FRAME_ACTIVITY = "com.jingdong.app.mall.MainFrameActivity";
     private static final String AD_ACTIVITY = "com.jingdong.app.mall.ad.ADActivity";
+    private static final String BASE_FRAME_UTIL = "com.jingdong.common.BaseFrameUtil";
 
     @Override
-    public void android_app_Activity__onResume__(ILTweaks.MethodParam param) {
-        afterOnClass(MAIN_ACTIVITY, param, () -> {
-            Activity activity = (Activity) param.thisObject;
-            new Handler().post(() -> {
-                try {
-                    if (!activity.isFinishing()) {
-                        Logger.v("Enter MainFrameActivity.");
-                        Intent intent = new Intent();
-                        intent.setClassName(activity, MAIN_FRAME_ACTIVITY);
-                        activity.startActivity(intent);
-                        activity.finish();
-                    }
-                } catch (Throwable e) {
-                    Logger.e("Can't enter MainFrameActivity.", e);
-                }
-            });
+    public void android_app_Activity__performCreate__Bundle_PersistableBundle(ILTweaks.MethodParam param) {
+        beforeOnClass(MAIN_FRAME_ACTIVITY, param, () -> {
+            Logger.d("Set needStartImage false.");
+            ReflectUtils.setStaticBooleanField(findClass(BASE_FRAME_UTIL), "needStartImage", false);
         });
     }
 
