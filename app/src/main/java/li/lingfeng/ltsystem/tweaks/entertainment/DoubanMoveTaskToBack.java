ea153511ffc068a0696a1295a2bcd758a161e11a
bluesky139 <bluesky139@gmail.com>
2020-05-04 10:44:20 +0800

    [Douban] Back to previous app if open Douban from other app.

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/DoubanMoveTaskToBack.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/DoubanMoveTaskToBack.java
@@ -1,6 +1,7 @@
 package li.lingfeng.ltsystem.tweaks.entertainment;
 
 import android.app.Activity;
+import android.net.Uri;
 
 import li.lingfeng.ltsystem.ILTweaks;
 import li.lingfeng.ltsystem.R;
@@ -12,18 +13,24 @@ import li.lingfeng.ltsystem.utils.Logger;
 @MethodsLoad(packages = PackageNames.DOUBAN, prefs = R.string.key_douban_move_task_to_back)
 public class DoubanMoveTaskToBack extends TweakBase {
 
-    private static final String FACADE_ACTIVITY = "com.douban.frodo.activity.FacadeActivity";
+    private static final String SPLASH_ACTIVITY = "com.douban.frodo.activity.SplashActivity";
+    private static final String REFERRER_DOUBAN = "android-app://" + PackageNames.DOUBAN;
     private int mCount = 0;
 
     @Override
     public void android_app_Activity__performCreate__Bundle_PersistableBundle(ILTweaks.MethodParam param) {
         param.after(() -> {
-            if (param.thisObject.getClass().getName().equals(FACADE_ACTIVITY)) {
+            Activity activity = (Activity) param.thisObject;
+            if (activity.getClass().getName().equals(SPLASH_ACTIVITY)) {
+                return;
+            }
+            Uri referrer = activity.getReferrer();
+            if (referrer != null && !referrer.toString().equals(REFERRER_DOUBAN)) {
                 mCount = 1;
-                Logger.d("MoveTaskToBack onCreate " + mCount + ", FacadeActivity.");
-            } else if (mCount == 1) {
+                Logger.d("MoveTaskToBack onCreate " + mCount + ", " + activity.getClass().getName());
+            } else if (mCount >= 1) {
                 ++mCount;
-                Logger.d("MoveTaskToBack onCreate " + mCount + ", " + param.thisObject.getClass().getName());
+                Logger.d("MoveTaskToBack onCreate " + mCount + ", " + activity.getClass().getName());
             }
         });
     }
@@ -34,7 +41,7 @@ public class DoubanMoveTaskToBack extends TweakBase {
             if (mCount == 1) {
                 Activity activity = (Activity) param.thisObject;
                 if (activity.isFinishing()) {
-                    Logger.d("MoveTaskToBack finishing last activity, " + param.thisObject.getClass().getName());
+                    Logger.d("MoveTaskToBack finishing last activity, " + activity.getClass().getName());
                     mCount = 0;
                     activity.moveTaskToBack(true);
                 }
