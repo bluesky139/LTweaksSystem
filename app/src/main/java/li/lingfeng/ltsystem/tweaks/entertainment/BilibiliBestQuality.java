5ed6d51415924e2f6d0bca8e1774a8051b55936e
bluesky139 <bluesky139@gmail.com>
2020-05-07 09:03:35 +0800

    [Bilibili] Compatible with v5.58.

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/BilibiliBestQuality.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/entertainment/BilibiliBestQuality.java
@@ -18,42 +18,54 @@ import li.lingfeng.ltsystem.utils.Logger;
 public class BilibiliBestQuality extends TweakBase {
 
     private static final String VIDEO_DETAILS_ACTIVITY = "tv.danmaku.bili.ui.video.VideoDetailsActivity";
+    private static final String BANGUMI_DETAIL_ACTIVITY = "com.bilibili.bangumi.ui.page.detail.BangumiDetailActivityV3";
     private static final String LIVE_ROOM_ACTIVITY = "com.bilibili.bililive.videoliveplayer.ui.roomv3.LiveRoomActivityV3";
 
     @Override
     public void android_app_Activity__performCreate__Bundle_PersistableBundle(ILTweaks.MethodParam param) {
         beforeOnClass(VIDEO_DETAILS_ACTIVITY, param, () -> {
-            Activity activity = (Activity) param.thisObject;
-            Intent intent = activity.getIntent();
-            Uri uri = activity.getIntent().getData();
-            if (uri == null) {
-                return;
-            }
-
-            Uri.Builder builder = uri.buildUpon().clearQuery();
-            for (String key : uri.getQueryParameterNames()) {
-                if (key.equals("player_preload")) {
-                    Logger.d("Remove player_preload.");
-                } else {
-                    builder.appendQueryParameter(key, uri.getQueryParameter(key));
-                }
-            }
-            uri = builder.build();
-            //Logger.d("new uri: " + uri);
-            intent.setData(uri);
-            intent.removeExtra("player_preload");
+            removePlayerPreload(param);
+        });
+        beforeOnClass(BANGUMI_DETAIL_ACTIVITY, param, () -> {
+            removePlayerPreload(param);
         });
         beforeOnClass(LIVE_ROOM_ACTIVITY, param, () -> {
-            Activity activity = (Activity) param.thisObject;
-            Intent intent = activity.getIntent();
-            String qualities = intent.getStringExtra("quality_description");
-            if (qualities != null) {
-                Logger.d("quality_description " + qualities);
-                JSONArray jArray = JSON.parseArray(qualities);
-                String maxQuality = jArray.getJSONObject(0).getString("qn");
-                Logger.d("best quality " + maxQuality);
-                intent.putExtra("current_qn", maxQuality);
-            }
+            setLiveRoomQuality(param);
         });
+    }
+
+    private void removePlayerPreload(ILTweaks.MethodParam param) {
+        Activity activity = (Activity) param.thisObject;
+        Intent intent = activity.getIntent();
+        Uri uri = activity.getIntent().getData();
+        if (uri == null) {
+            return;
+        }
+
+        Uri.Builder builder = uri.buildUpon().clearQuery();
+        for (String key : uri.getQueryParameterNames()) {
+            if (key.equals("player_preload")) {
+                Logger.d("Remove player_preload.");
+            } else {
+                builder.appendQueryParameter(key, uri.getQueryParameter(key));
+            }
+        }
+        uri = builder.build();
+        //Logger.d("new uri: " + uri);
+        intent.setData(uri);
+        intent.removeExtra("player_preload");
+    }
+
+    private void setLiveRoomQuality(ILTweaks.MethodParam param) {
+        Activity activity = (Activity) param.thisObject;
+        Intent intent = activity.getIntent();
+        String qualities = intent.getStringExtra("quality_description");
+        if (qualities != null) {
+            Logger.d("quality_description " + qualities);
+            JSONArray jArray = JSON.parseArray(qualities);
+            String maxQuality = jArray.getJSONObject(0).getString("qn");
+            Logger.d("best quality " + maxQuality);
+            intent.putExtra("current_qn", maxQuality);
+        }
     }
 }
