4859c7123452fd88351df31664355eaf26522ddc
bluesky139 <bluesky139@gmail.com>
2020-07-23 10:08:01 +0800

    [Debug] Set FLAG_DEBUGGABLE at startProcessLocked().

--- app/src/main/java/li/lingfeng/ltsystem/tweaks/system/Debuggable.java
+++ app/src/main/java/li/lingfeng/ltsystem/tweaks/system/Debuggable.java
@@ -1,10 +1,8 @@
 package li.lingfeng.ltsystem.tweaks.system;
 
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageParser;
 
 import li.lingfeng.ltsystem.ILTweaks;
-import li.lingfeng.ltsystem.LTHelper;
 import li.lingfeng.ltsystem.R;
 import li.lingfeng.ltsystem.lib.MethodsLoad;
 import li.lingfeng.ltsystem.prefs.PackageNames;
@@ -15,19 +13,14 @@ import li.lingfeng.ltsystem.utils.Logger;
 public class Debuggable extends TweakBase {
 
     @Override
-    public void android_content_pm_PackageParser__parsePackage__File_int_boolean(ILTweaks.MethodParam param) {
-        param.after(() -> {
-            PackageParser.Package pkg = (PackageParser.Package) param.getResult();
-            if (pkg == null) {
-                return;
+    public void com_android_server_am_ProcessList__startProcessLocked__String_ApplicationInfo_boolean_int_HostingRecord_boolean_boolean_int_boolean_String_String_String$array_Runnable(ILTweaks.MethodParam param) {
+        param.before(() -> {
+            ApplicationInfo info = (ApplicationInfo) param.args[1];
+            if (info.packageName.equals(PackageNames.ANDROID)) {
+                Logger.i("Set FLAG_DEBUGGABLE at startProcessLocked() for all apps.");
+            } else {
+                info.flags |= ApplicationInfo.FLAG_DEBUGGABLE;
             }
-            if (pkg.packageName.equals(PackageNames.ANDROID)) {
-                Logger.i("Set debuggable for all apps.");
-                return;
-            }
-
-            ApplicationInfo appInfo = pkg.applicationInfo;
-            appInfo.flags |= ApplicationInfo.FLAG_DEBUGGABLE;
         });
     }
 }
